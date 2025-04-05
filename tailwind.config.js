f7ebadb1ef812f7e922999c1929d967ad281f9d9
David Stetler <fifth5hand@gmail.com>
2025-04-05 12:42:24 -0400

    Add Tailwind CSS configuration

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'neutral-dark': '#121212',
+        'whte': '#f5f7fa',
+        'neutral-white': '#ffffff',
+        'bg': '#f5f7fa',
+        'white': '#ffffff',
+        'primary-colour': '#a259ff',
+        'caption-and-label-text': '#858584',
+        'text': '#ffffff',
+        'background': '#2b2b2b',
+      }
+    }
+  },
+  plugins: [],
+}
